2592ce2709f6098b62938510809a9ad81a306c89
hontatfai <hontatfai@hotmail.com>
2018-04-05 17:02:31 -0700

    install libs, npm install react-native-simple-toast --save && npm install ac-qrcode --save

---
+++ src/components/Scan.js
@@ -0,0 +1,9 @@
+/**
+ * Create QR Code Scanning Function
+ * by Dahui Han on 4/5/2018
+ */
+
+// Import Libraries
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+import Toast from 'react-native-simple'

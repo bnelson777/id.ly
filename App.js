138642bab21abdacd9acbddb4341f272d2e67193
Nuni88 <jgraham@pdx.edu>
2018-04-06 16:16:08 -0700

    Revert to original

--- App.js
+++ App.js
@@ -1,15 +1,23 @@
-import React, { Component } from 'react';
-import { Provider } from 'react-redux';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
-import store from './app/store';
-import Main from './app/index';
-
-export default class App extends Component {
-    render() {
-        return (
-            <Provider store={store}>
-                <Main />
-            </Provider>
-        );
-    }
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
+      </View>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});

cfb3180dc2cb4bd7fa5cfda23c5f7bc1bc0d83f5
Vance Vescogni <vance@Vances-MacBook-Air.local>
2018-04-08 23:25:40 -0700

    removes add button

--- app/components/rolodex.js
+++ app/components/rolodex.js
@@ -133,25 +133,4 @@ const styles = StyleSheet.create({
         marginTop: 5,
         fontSize: 14,
     },
-
-    addButton: {
-        backgroundColor: '#ff5722',
-        borderColor: '#ff5722',
-        borderWidth: 1,
-        height: 50,
-        width: 50,
-        borderRadius: 50 / 2,
-        alignItems: 'center',
-        justifyContent: 'center',
-        position: 'absolute',
-        bottom: 20,
-        right: 20,
-        shadowColor: "#000000",
-        shadowOpacity: 0.8,
-        shadowRadius: 2,
-        shadowOffset: {
-            height: 1,
-            width: 0
-        }
-    }
 });

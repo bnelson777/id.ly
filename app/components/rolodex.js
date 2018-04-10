2115c63bbebb8755c39f8be820c5b9651fd76395
Vance Vescogni <vance@Vances-MacBook-Air.local>
2018-04-09 19:42:49 -0700

    adds popups

--- app/components/rolodex.js
+++ app/components/rolodex.js
@@ -1,27 +1,20 @@
 import React, { Component } from 'react';
 import {
+    Alert,
     StyleSheet,
-    FlatList,
     View,
     Text,
+    FlatList,
+    TouchableOpacity,
     ActivityIndicator, TouchableHighlight, ActionSheetIOS
 } from 'react-native';
 
-import {bindActionCreators} from 'redux';
-import { connect } from 'react-redux';
-
-import * as ReduxActions from '../actions'; //Import your actions
-
 import {Actions} from 'react-native-router-flux'
 
-//Buttons for Action Sheet
-const BUTTONS = [
-    "Edit",
-    "Delete",
-    'Cancel',
-];
+import { connect } from 'react-redux';
 
-const CANCEL_INDEX = 2;
+import {bindActionCreators} from 'redux';
+
 
 class Rolodex extends Component {
     constructor(props) {
@@ -30,46 +23,53 @@ class Rolodex extends Component {
         this.state = {};
 
         this.renderItem = this.renderItem.bind(this);
-        this.showOptions = this.showOptions.bind(this);
     }
 
-    componentDidMount() {
-        this.props.getContact(); //call our action
-    }
-
-    showOptions(name) {
-        ActionSheetIOS.showActionSheetWithOptions({
-                options: BUTTONS,
-                cancelButtonIndex: CANCEL_INDEX,
-                destructiveButtonIndex: 1,
-            },
-            (buttonIndex) => {
-                if (buttonIndex === 0) Actions.new_name({name: name, edit: true, title:"Edit name"})
-            });
+    // Dummy function for button presses
+    pressButton(label){
+        Alert.alert(label);
     }
 
     render() {
         return (
             <View style={styles.container}>
                 <FlatList
-                    ref='listRef'
-                    data={this.props.contact}
-                    renderItem={this.renderItem}
-                    keyExtractor={(item, index) => index}/>
-
-
-                <TouchableHighlight style={styles.addButton}
-                                    underlayColor='#ff7043' onPress={() => Actions.new_name()}>
-                    <Text style={{fontSize: 25, color: 'white'}}>+</Text>
-                </TouchableHighlight>
+                        ref='listRef'
+                        data={this.props.contact}
+                        renderItem={this.renderItem}
+                        keyExtractor={(item, index) => index}/>
             </View>
         );
     }
+    //             <TouchableHighlight onPress={() => {this.pressButton("Contact Info")}}>
+    //                 <Text style={{fontSize: 25, color: 'white'}}>+</Text>
+    //             </TouchableHighlight>
+    //         </View>
+    //     );
+    // }
     
 
     renderItem({item, index}) {
         return (
-            <TouchableHighlight onPress={() => this.showOptions(item)} underlayColor='rgba(0,0,0,.2)'>
+
+            // <View
+            //         style={styles.button}
+            //         width={screenWidth * 0.6}
+            //         marginLeft={screenWidth * 0.05}
+            //         marginRight={screenWidth * 0.05}
+            //         backgroundColor={COLORS[index % COLORS.length]}
+            //     >
+            //         <TouchableOpacity onPress={() => {this.pressButton(item.name)}}>
+            //             <Text>{item.name}</Text>
+            //         </TouchableOpacity>
+            //     </View>
+
+            // <TouchableOpacity onPress={() => {this.pressButton(item.name)}}>
+            //             <Text>{item.name}</Text>
+            //         </TouchableOpacity>
+
+
+            <TouchableHighlight onPress={() => {this.pressButton(item.name)}}>
                 <View style={styles.row}>
                     <Text style={styles.name}>
                         {item.name}
@@ -84,24 +84,14 @@ class Rolodex extends Component {
 };
 
 
-
-// The function takes data from the app current state,
-// and insert/links it into the props of our component.
-// This function makes Redux know that this component needs to be passed a piece of the state
 function mapStateToProps(state, props) {
     return {
-            contact: state.dataReducer.contact
+        contact: state.dataReducer.contact
     }
 }
 
-// Doing this merges our actions into the component’s props,
-// while wrapping them in dispatch() so that they immediately dispatch an Action.
-function mapDispatchToProps(dispatch) {
-    return bindActionCreators(ReduxActions, dispatch);
-}
+export default connect(mapStateToProps)(Rolodex);
 
-//Connect everything
-export default connect(mapStateToProps, mapDispatchToProps)(Rolodex);
 
 const styles = StyleSheet.create({
 

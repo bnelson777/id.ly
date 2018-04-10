e137b955e2b3b18282bd2b1ad96d8a4c604c8312
ad25 <ad23@pdx.edu>
2018-04-10 01:01:22 -0700

    replaced rolodex as scene in router

--- app/index.js
+++ app/index.js
@@ -54,6 +54,7 @@ class Main extends Component{
         return (
             <Router>
                 <Scene key="root">
+                    <Scene key="rolodex" component={Rolodex} title="Rolodex"/>
                     <Scene key="home" component={Home} title="Home"/>
                     <Scene key="wallet" component={Wallet} title="Wallet"/>
                     <Scene key="share" component={Share} title="Share"/>

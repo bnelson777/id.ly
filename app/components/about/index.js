977966f3e230bcf9c20c9735096002c300c059b2
hontatfai <hontatfai@hotmail.com>
2018-04-14 14:04:04 -0700

    create the index.js of about page

---
+++ app/components/about/index.js
@@ -0,0 +1,28 @@
+/**
+ * Create About Page
+ * by id.ly Team on 4/14/2018
+ */
+
+//Import Libraries
+import React, {Component} from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import {bindActionCreators} from 'redux';
+import {connect} from 'react-redux';
+import * as ReduxActions from '../../actions';
+import {Actions} from 'react-native-router-flux';
+
+class About extends Component {
+
+}
+
+function mapStateToProps(state, props) {
+    return {
+        cards: state.dataReducer.cards
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators(ReduxActions, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(About);

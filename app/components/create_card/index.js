cbc646e2d99d0d13a40fe6513c2bc53335f912ac
ad25 <ad23@pdx.edu>
2018-04-13 23:29:38 -0700

    Layout created with dummy information

--- app/components/create_card/index.js
+++ app/components/create_card/index.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 
 import {
     Text,
+    View,
+    TouchableOpacity,
+    Image,
+    FlatList,
+    TextInput
 } from 'react-native';
 import styles from './styles';
 
@@ -9,12 +14,54 @@ class CreateCard extends Component {
     constructor(props) {
         super(props);
 
-        this.state = {};
+        // could be an issue if title is already defined
+        // but that could be handled when you add a field to the form
+        this.state = {form: [{title:"Label"}, {title:"Name"}, {title:"Email"}]};
     }
 
     render() {
         return (
-            <Text> create card screen </Text>
+            <View style={styles.bodyContainer}>
+                <View style={styles.topButtonContainer}>
+                    <TouchableOpacity onPress={() => {alert('')}}>
+                        <Text style={styles.buttonText}>Cancel</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => {alert('')}}>
+                        <Text style={styles.buttonText}>Add</Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.screenContainer}>
+                    <View style={styles.formContainer}>
+                        <FlatList
+                        data={this.state.form}
+                        keyExtractor={item => item.title}
+                        renderItem={this.renderItem}
+                        />
+                        <View style={styles.addFieldButton}>
+                            <TouchableOpacity onPress={() => {alert('adding field to list!')}}>
+                                <Text style={styles.buttonText}> Add field </Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                    <View style={styles.addImageContainer}>
+                        <TouchableOpacity onPress={() => {alert('')}}>
+                            <Image source={require('../../assets/person.png')} />
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </View>
+        );
+    }
+    renderItem = ({item, index}) => {
+        return (
+            <View style={styles.formItemContainer}>
+                <Text style={styles.formTitle}> {item.title} </Text>
+                <TextInput
+                    style={styles.formInput}
+                    placeholder={"Enter " + item.title}
+                    underlineColorAndroid="transparent"
+                />
+            </View>
         );
     }
 };
--- app/components/create_card/styles.js
+++ app/components/create_card/styles.js
@@ -1,5 +1,50 @@
-import { StyleSheet } from 'react-native';
-import { COLOR_PRIMARY, COLOR_SECONDARY } from './../styles/common';
+import { StyleSheet, Dimensions } from 'react-native';
+import { COLOR_PRIMARY, COLOR_SECONDARY, FONT_SIZE, FONT_WEIGHT, FONT_COLOR, FONT_NORMAL, BACKGROUND_COLOR, BORDER_RADIUS, BORDER_WIDTH } from './../styles/common';
+        
+const SCREEN_WIDTH = Dimensions.get('window').width;
 
 export default StyleSheet.create({
+    bodyContainer: {
+        flex: 1,
+        backgroundColor: BACKGROUND_COLOR
+    },
+    topButtonContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    screenContainer: {
+        flex: 10,
+        flexDirection: 'row',
+    },
+    inputContainer: {
+        flex: 1
+    },
+    formContainer: {
+        flex: 3,
+    },
+    formItemContainer: {
+        margin: '1%'
+    },
+    formTitle: {
+    },
+    formInput: {
+        borderRadius: BORDER_RADIUS,
+        margin: '5%'
+    },
+    addImageContainer: {
+        flex: 2,
+        alignItems: 'center'
+    },
+    addFieldButton: {
+        flex: 0.2,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    buttonText: {
+        padding: '5%',
+        fontSize: 20,
+        color: 'blue'
+    },
 });

c289fef9c5725565140e33d9b9f27b131729371e
hontatfai <hontatfai@hotmail.com>
2018-04-16 02:42:30 -0700

    created the simple about page

--- app/components/about/index.js
+++ app/components/about/index.js
@@ -5,14 +5,48 @@
 
 //Import Libraries
 import React, {Component} from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, Button, TouchableHighlight,
+        StyleSheet, Image, Linking } from 'react-native';
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
 import * as ReduxActions from '../../actions';
 import {Actions} from 'react-native-router-flux';
+import styles from './styles';
 
 class About extends Component {
-
+    render() {
+        return(
+            <View style = {styles.container}>
+                <View style = {styles.column}/>
+                <View>
+                    <Image style = {styles.image}
+                         source = {require('../../assets/id_ly.png')}/>
+                </View>
+                <View style = {styles.row}>
+                    <View style={styles.icon}>
+                        <TouchableHighlight
+                            onPress={()=>{ Linking.openURL('https://www.pdx.edu')}}>
+                            <Image style={styles.icon} 
+                                source={require('../../assets/contact.png')} />
+                        </TouchableHighlight>
+                    </View>
+                    <View style={styles.icon}/>
+                    <View style={styles.icon}>
+                        <TouchableHighlight
+                            onPress={()=>{ Linking.openURL('https://github.com/bnelson777/id.ly')}}>
+                            <Image style={styles.icon} 
+                                source={require('../../assets/GitHub.png')} />
+                        </TouchableHighlight>
+                    </View>
+                </View>
+                <View>
+                    <Text style = {styles.words}>
+                        Powered by id.ly team.  All right reserved.
+                    </Text>
+                </View>
+            </View>
+        );
+    }
 }
 
 function mapStateToProps(state, props) {

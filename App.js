a6e737035d68fdf6cf5015d03b518d31c41be8ef
hontatfai <hontatfai@hotmail.com>
2018-04-24 00:16:18 -0700

    disable the Warning with 'componentWill'

--- App.js
+++ App.js
@@ -1,9 +1,17 @@
+/**
+ * Create App Page
+ * by id.ly Team
+ */
+
+//Import Libraries
 import React, { Component } from 'react';
-import { Platform } from 'react-native';
+import { Platform, YellowBox } from 'react-native';
 import { Provider } from 'react-redux';
 import { Font } from 'expo';
 import store from './app/store';
 import Main from './app/index';
+
+console.ignoredYellowBox = ['Warning: componentWill'];
 
 export default class App extends Component {
     constructor() {
@@ -15,9 +23,9 @@ export default class App extends Component {
     
     async componentDidMount() {
         if (Platform.OS === 'ios') {
-          await Font.loadAsync({
-            'sans-serif': require('./app/assets/fonts/sansserif.ttf'),
-          });
+            await Font.loadAsync({
+                'sans-serif': require('./app/assets/fonts/sansserif.ttf'),
+            });
         }
         this.setState({loadingFont: true});
     }

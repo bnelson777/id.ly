3c79d21b9c5e6cdcbaa910cd52e10c6da86b9af9
Nuni88 <jgraham@pdx.edu>
2018-04-29 15:25:14 -0700

    Fixed photo sometimes still showing as square

--- app/components/card_list/styles.js
+++ app/components/card_list/styles.js
@@ -33,7 +33,7 @@ export default StyleSheet.create({
     imageContainer: {
         width: buttonHeight,
         height: buttonHeight,
-        resizeMode: 'contain'
+        resizeMode: 'cover'
     },
 
     portrait: {

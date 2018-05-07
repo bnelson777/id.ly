480c52a10a0eb78ba18e79be1f3100f0e4c473b9
ad25 <ad23@pdx.edu>
2018-05-06 22:26:13 -0700

    Removed lines from MainApplication to allow for testing of react-native link

--- android/app/src/main/java/com/id/ly/MainApplication.java
+++ android/app/src/main/java/com/id/ly/MainApplication.java
@@ -3,8 +3,6 @@ package com.id.ly;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.lwansbrough.RCTCamera.RCTCameraPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -24,9 +22,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new VectorIconsPackage(),
-            new RCTCameraPackage()
+          new MainReactPackage()
       );
     }
 

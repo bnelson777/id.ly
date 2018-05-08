288788a61e1688b98a31064c5dd7f7bc470f9cd6
hontatfai <hontatfai@hotmail.com>
2018-05-07 19:37:02 -0700

    changed the directory name to idly

--- android/app/src/main/java/com/idly/MainActivity.java
+++ android/app/src/main/java/com/idly/MainActivity.java
@@ -1,4 +1,4 @@
-package com.id.ly;
+package com.idly;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        return "id.ly";
+        return "idly";
     }
 }

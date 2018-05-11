47f539eff63eb4efd3eff4e41fc3e8133c6a4ff1
Nuni88 <jgraham@pdx.edu>
2018-05-11 14:42:53 -0700

    Added file initialization

--- App.js
+++ App.js
@@ -7,17 +7,38 @@
 import React, { Component } from 'react';
 import { Platform, AppRegistry, YellowBox } from 'react-native';
 import { Provider } from 'react-redux';
+import RNFetchBlob from 'react-native-fetch-blob';
 import store from './app/store';
 import Main from './app/index';
+
+export const fileDir = RNFetchBlob.fs.dirs.DocumentDir + '/idly/';
 
 console.ignoredYellowBox = ['Warning: componentWill'];
 
 export default class App extends Component {
     constructor() {
         super();
+        this.initFiles();
         this.state = {
           loadingFont: false,
         }
+    }
+
+    initFiles(){
+        RNFetchBlob.fs.mkdir(fileDir)
+            .catch((err) => {});
+        RNFetchBlob.fs.createFile(
+            fileDir + 'cards.dat',
+            '',
+            'utf8'
+        )
+            .catch((err) => {});
+        RNFetchBlob.fs.createFile(
+            fileDir + 'messages.dat',
+            '',
+            'utf8'
+        )
+            .catch((err) => {});
     }
     
     async componentDidMount() {
